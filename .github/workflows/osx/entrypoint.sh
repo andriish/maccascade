5e7e3557e1ee80c70b45e3347d06df17668eaf9d
Alois Kabelschacht <andrii.verbytskyi@mpp.mpg.de>
2021-06-21 13:13:54 +0200

    OK

--- .github/workflows/osx/entrypoint.sh
+++ .github/workflows/osx/entrypoint.sh
@@ -18,6 +18,7 @@ brew install pkg-config
 brew install --cask basictex
 eval "$(/usr/libexec/path_helper)"
 export PATH=$PATH:/usr/local/bin:/usr/local//Cellar/gcc/11.1.0_1/libexec/gcc/x86_64-apple-darwin19/11.1.0/:/Library/TeX/texbin/
+sudo tlmgr update --self
 sudo tlmgr install sectsty
 
 which -a gfortran
