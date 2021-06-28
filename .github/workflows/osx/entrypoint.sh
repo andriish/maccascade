002b4763afa07f185c8f86951054b73d5e0eb100
andriish <andriish@users.noreply.github.com>
2021-06-28 13:15:27 +0200

    Update entrypoint.sh

--- .github/workflows/osx/entrypoint.sh
+++ .github/workflows/osx/entrypoint.sh
@@ -8,20 +8,18 @@ brew install wget coreutils
 brew install gsl
 brew install gnu-sed
 brew install gcc
+brew install cmake
 brew install zlib 
 brew install autoconf 
 brew install automake 
 brew install libtool 
 brew install pkg-config
-brew install --build-from-source --cc=gcc-11 lhapdf
-brew install --build-from-source --cc=gcc-11 hepmc 
-brew install --build-from-source --cc=gcc-11 hepmc3 
-brew install --build-from-source --cc=gcc-11 pythia 
 brew install --cask basictex
 eval "$(/usr/libexec/path_helper)"
 export PATH=$PATH:/usr/local/bin:/usr/local//Cellar/gcc/11.1.0_1/libexec/gcc/x86_64-apple-darwin19/11.1.0/:/Library/TeX/texbin/
 sudo tlmgr update --self
 sudo tlmgr install sectsty collection-fontsrecommended
+#####
 
 which gfortran-11
 if [ "$?" = "0" ]; then 
@@ -37,7 +35,14 @@ else
 fi
 export CXXFLAGS=-std=c++14
 cp /usr/local/bin/gfortran-11 /usr/local/bin/gfortran
-
+########
+wget  https://www.hepforge.org/archive/lhapdf/LHAPDF-6.2.1.tar.gz
+tar zxvf LHAPDF-6.2.1.tar.gz
+cd LHAPDF-6.2.1
+./configure --prefix=/usr/local
+make -j 2  install
+cd ..
+########
 wget https://tmdlib.hepforge.org/downloads/tmdlib-2.2.01.tar.gz
 tar zxfv tmdlib-2.2.01.tar.gz 
 cd tmdlib-2.2.01
@@ -45,12 +50,29 @@ cd tmdlib-2.2.01
 make -j 2 
 make install
 cd ..
+###########
+wget https://gitlab.cern.ch/hepmc/HepMC/-/archive/2.06.11/HepMC-2.06.11.tar.gz
+tar zxfv HepMC-2.06.11.tar.gz
+cd HepMC-2.06.11
+cmake -Dmomentum=GEV -Dlength=MM CMakeLists.txt
+make -j 2
+make install 
+cd ..
+#############
+wget https://pythia.org/download/pythia82/pythia8243.tgz
+tar zxfv pythia8243.tgz
+cd pythia8243
+./configure --refix=/usr/local --enable-shared
+make -j 2
+make install
+cd ..
+#################
 brew install root
 git clone https://gitlab.cern.ch/averbyts/cascade
 cd cascade
 git checkout hepmc3
 autoreconf -fisv
-#this will enable hepmc3 only
+#this will enable hepmc3 only if it is present
 ./configure --prefix=$(pwd)/TESTINSTALLDIR --with-hepmc=/usr/local --with-hepmc3=/usr/local  --with-tmdlib=/usr/local --with-lhapdf=/usr/local --with-zlib=/usr/local/opt/zlib --with-gsl=/usr/local --with-pythia8=/usr/local 
 make -j 2
 make install
